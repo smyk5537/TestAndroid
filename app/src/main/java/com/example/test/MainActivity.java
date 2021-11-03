4f203493c3538109d934aa25873f8b0663c3771c
smyk5337 <ykdooley@hanmail.net>
2021-11-03 21:12:17 +0900

    황유경 push Test

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //황유경 push test
     }
 }
